1a0e5080d32fa019383c3eb6724d4bb13e7a3e92
Dimitri-Y <dimoon.bond@gmail.com>
2023-05-06 10:55:01 +0300

    update js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = event => {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+const onPlayThrottle = throttle(onPlay, 1000);
+
+player.on('timeupdate', onPlayThrottle);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+//   .then(function (seconds) {})
+//   .catch(function (error) {
+//     switch (error.name) {
+//       case 'RangeError':
+//         break;
+//       default:
+//         break;
+//     }
+//   });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const input = document.querySelector('input');
+const message = document.querySelector('textarea');
+const submitForm = document.querySelector('form');
+
+const inputChanged = event => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: event.currentTarget.value, message: message.value })
+  );
+};
+const messageChanged = event => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email: input.value, message: event.currentTarget.value })
+  );
+};
+
+const submitBtnClick = event => {
+  localStorage.clear();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+};
+const updateBrowserform = () => {
+  formElements = submitForm.elements;
+  try {
+    let { email, message } = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    );
+    if (email === undefined || message === undefined) {
+      formElements.email.value = '';
+      formElements.message.value = '';
+    } else {
+      formElements.email.value = email;
+      formElements.message.value = message;
+    }
+  } catch (error) {}
+};
+updateBrowserform();
+input.addEventListener('input', throttle(inputChanged, 500));
+message.addEventListener('input', throttle(messageChanged, 500));
+submitForm.addEventListener('submit', submitBtnClick);
